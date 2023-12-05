778fecd9e95af2ebeadce8baf7956e7af262b2b0
VladislavKornyakov <vladislav.kornyakov@vrpconsulting.com>
2023-12-05 10:03:30 +0300

    Add some changes

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели');
+'use strict';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
 console.log(numberOfFilms);
 
 const personalMovieDB = {
@@ -8,3 +9,12 @@ const personalMovieDB = {
 	genres : [],
 	privat : false
 };
+const lastWatchedMovie1 = prompt('Один из последних просмотреных фильмов?', '');
+const movieRating1 = prompt('На сколько оцените его ?', '');
+const lastWatchedMovie2 = prompt('Один из последних просмотреных фильмов?', '');
+const movieRating2 = prompt('На сколько оцените его ?', '');
+
+personalMovieDB.movies[lastWatchedMovie1] =  movieRating1;
+personalMovieDB.movies[lastWatchedMovie2] =  movieRating2;
+
+console.log(personalMovieDB);
